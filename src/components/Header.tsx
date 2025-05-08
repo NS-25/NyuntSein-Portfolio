021b5cd5c19276039c50d9cf499de125ae84cfa7
George-25 <georgemallory26@gmail.com>
2025-05-07 20:56:17 -0400

    added some css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,28 @@
 const Header = () => {
   return (
-    <header>
-      <div className="container">
-        <div className="font-bold text-xl">
-          <a href="#home">Nyunt Sein</a>
-        </div>
-        <div className="menu">
-          <ul className="flex space-x-8">
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#skills">Skills</a>
-            </li>
-            <li>
-              <a href="#project">Projects</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
+    <header className="flex flex-row justify-between items-center ">
+      <div className="font-bold text-xl">
+        <a href="#home">Nyunt Sein</a>
       </div>
+      <nav className="menu">
+        <ul className="flex space-x-8">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#project">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };

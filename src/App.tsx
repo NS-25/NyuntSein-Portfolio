8af46be01c05a04178d14b990833d0cbf8f512b2
George-25 <georgemallory26@gmail.com>
2025-05-07 22:54:21 -0400

    added some codes'

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,113 @@ function App() {
     <>
       <div>
         <Header />
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        impedit commodi enim, architecto eligendi tenetur! Quae rerum molestiae,
+        sed hic, blanditiis nisi debitis distinctio adipisci nam maiores atque
+        cum doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Culpa, ab nobis fuga atque cum quisquam minima dolorem autem porro
+        doloremque repudiandae explicabo mollitia vel enim earum alias.
+        Molestiae, porro nulla. Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Odit cum inventore accusantium reiciendis et! Mollitia
+        sapiente ex dignissimos modi magni odit rem iure, minima facilis
+        accusamus at, provident ullam a? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Vel, exercitationem molestiae. Non aliquid explicabo
+        eaque hic minima excepturi sit dolores dolorem nostrum consequuntur,
+        autem nobis! Atque esse facere dicta nostrum. Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Repellendus optio velit illo vel minima
+        repudiandae molestiae illum, itaque earum fugit doloribus commodi, totam
+        similique iste quia veniam facere, perspiciatis asperiores! Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Praesentium beatae, earum
+        ipsa, quos exercitationem eaque voluptate commodi sunt delectus
+        provident omnis temporibus excepturi, eum quibusdam modi? Ex
+        consequuntur animi unde? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Vel, exercitationem molestiae. Non aliquid explicabo
+        eaque hic minima excepturi sit dolores dolorem nostrum consequuntur,
+        autem nobis! Atque esse facere dicta nostrum. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Vel, exercitationem molestiae. Non aliquid
+        explicabo eaque hic minima excepturi sit dolores dolorem nostrum
+        consequuntur, autem nobis! Atque esse facere dicta nostrum. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Vel, exercitationem
+        molestiae. Non aliquid explicabo eaque hic minima excepturi sit dolores
+        dolorem nostrum consequuntur, autem nobis! Atque esse facere dicta
+        nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+        exercitationem molestiae. Non aliquid explicabo eaque hic minima
+        excepturi sit dolores dolorem nostrum consequuntur, autem nobis! Atque
+        esse facere dicta nostrum. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Vel, exercitationem molestiae. Non aliquid explicabo
+        eaque hic minima excepturi sit dolores dolorem nostrum consequuntur,
+        autem nobis! Atque esse facere dicta nostrum. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Repellat impedit commodi enim,
+        architecto eligendi tenetur! Quae rerum molestiae, sed hic, blanditiis
+        nisi debitis distinctio adipisci nam maiores atque cum doloribus. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Culpa, ab nobis fuga
+        atque cum quisquam minima dolorem autem porro doloremque repudiandae
+        explicabo mollitia vel enim earum alias. Molestiae, porro nulla. Lorem
+        ipsum, dolor sit amet consectetur adipisicing elit. Odit cum inventore
+        accusantium reiciendis et! Mollitia sapiente ex dignissimos modi magni
+        odit rem iure, minima facilis accusamus at, provident ullam a? Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Vel, exercitationem
+        molestiae. Non aliquid explicabo eaque hic minima excepturi sit dolores
+        dolorem nostrum consequuntur, autem nobis! Atque esse facere dicta
+        nostrum. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Repellendus optio velit illo vel minima repudiandae molestiae illum,
+        itaque earum fugit doloribus commodi, totam similique iste quia veniam
+        facere, perspiciatis asperiores! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Praesentium beatae, earum ipsa, quos exercitationem
+        eaque voluptate commodi sunt delectus provident omnis temporibus
+        excepturi, eum quibusdam modi? Ex consequuntur animi unde? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Vel, exercitationem
+        molestiae. Non aliquid explicabo eaque hic minima excepturi sit dolores
+        dolorem nostrum consequuntur, autem nobis! Atque esse facere dicta
+        nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+        exercitationem molestiae. Non aliquid explicabo eaque hic minima
+        excepturi sit dolores dolorem nostrum consequuntur, autem nobis! Atque
+        esse facere dicta nostrum. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Vel, exercitationem molestiae. Non aliquid explicabo
+        eaque hic minima excepturi sit dolores dolorem nostrum consequuntur,
+        autem nobis! Atque esse facere dicta nostrum. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Vel, exercitationem molestiae. Non aliquid
+        explicabo eaque hic minima excepturi sit dolores dolorem nostrum
+        consequuntur, autem nobis! Atque esse facere dicta nostrum. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Vel, exercitationem
+        molestiae. Non aliquid explicabo eaque hic minima excepturi sit dolores
+        dolorem nostrum consequuntur, autem nobis! Atque esse facere dicta
+        nostrum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Repellat impedit commodi enim, architecto eligendi tenetur! Quae rerum
+        molestiae, sed hic, blanditiis nisi debitis distinctio adipisci nam
+        maiores atque cum doloribus. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Culpa, ab nobis fuga atque cum quisquam minima dolorem
+        autem porro doloremque repudiandae explicabo mollitia vel enim earum
+        alias. Molestiae, porro nulla. Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Odit cum inventore accusantium reiciendis et! Mollitia
+        sapiente ex dignissimos modi magni odit rem iure, minima facilis
+        accusamus at, provident ullam a? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Vel, exercitationem molestiae. Non aliquid explicabo
+        eaque hic minima excepturi sit dolores dolorem nostrum consequuntur,
+        autem nobis! Atque esse facere dicta nostrum. Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Repellendus optio velit illo vel minima
+        repudiandae molestiae illum, itaque earum fugit doloribus commodi, totam
+        similique iste quia veniam facere, perspiciatis asperiores! Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Praesentium beatae, earum
+        ipsa, quos exercitationem eaque voluptate commodi sunt delectus
+        provident omnis temporibus excepturi, eum quibusdam modi? Ex
+        consequuntur animi unde? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Vel, exercitationem molestiae. Non aliquid explicabo
+        eaque hic minima excepturi sit dolores dolorem nostrum consequuntur,
+        autem nobis! Atque esse facere dicta nostrum. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Vel, exercitationem molestiae. Non aliquid
+        explicabo eaque hic minima excepturi sit dolores dolorem nostrum
+        consequuntur, autem nobis! Atque esse facere dicta nostrum. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Vel, exercitationem
+        molestiae. Non aliquid explicabo eaque hic minima excepturi sit dolores
+        dolorem nostrum consequuntur, autem nobis! Atque esse facere dicta
+        nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+        exercitationem molestiae. Non aliquid explicabo eaque hic minima
+        excepturi sit dolores dolorem nostrum consequuntur, autem nobis! Atque
+        esse facere dicta nostrum. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Vel, exercitationem molestiae. Non aliquid explicabo
+        eaque hic minima excepturi sit dolores dolorem nostrum consequuntur,
+        autem nobis! Atque esse facere dicta nostrum.
       </div>
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,47 @@
+import React, { useState, useEffect } from "React";
+import "./Header.css";
+
 const Header = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleMenu = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <header className="flex flex-row justify-between items-center ">
-      <div className="font-bold text-xl">
-        <a href="#home">Nyunt Sein</a>
-      </div>
-      <nav className="menu">
-        <ul className="flex space-x-8">
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#skills">Skills</a>
-          </li>
-          <li>
-            <a href="#project">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className="st-header flex flex-row justify-between items-center bg-white/80 backdrop-blur-md border-b py-4 px-2">
+        <div className="font-bold text-xl">
+          <a href="#home">Nyunt Sein</a>
+        </div>
+        <nav className="st-menu">
+          {expanded ? (
+            <ul className="st-main-menu flex space-x-8">
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#skills">Skills</a>
+              </li>
+              <li>
+                <a href="#project">Projects</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          ) : (
+            <button className="st-menu-icon">
+              {" "}
+              <i className="fa-regular fa-bars"></i>{" "}
+            </button>
+          )}
+        </nav>
+      </header>
+    </>
   );
 };
 
